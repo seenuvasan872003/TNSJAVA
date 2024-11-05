7c282ca5e239e54807546c7781f5fcecdec9c97d
My Name <seenuifo.2003@gmail.com>
2024-11-05 09:45:16 +0530

    adding the inheritance

---
+++ single_level_inheritance/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module single_level_inheritance {
+}
---
+++ single_level_inheritance/src/single_inheritance/SLInheritanceDemo.java
@@ -0,0 +1,8 @@
+//Program to demonstrate Single Inheritance 
+package single_inheritance;
+public class SLInheritanceDemo {
+public static void main(String[] args) {
+Student student = new Student("Seenuvasan", "8080808080", "villupuram", 9342238388L, 104, "IFET College");
+System.out.println(student);
+}
+}
